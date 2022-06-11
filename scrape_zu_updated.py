828c2cc2fe0de6d3b1d2b979fa26b0c605b6f520
Suhail AR Chougule <sarc007@msn.com>
2022-06-11 23:49:21 +0500

    Add partial links list to be saved and error messages / handling

--- scrape_zu_updated.py
+++ scrape_zu_updated.py
@@ -1,13 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
 from urllib.parse import urlparse
+
+import requests
+import validators
+from bs4 import BeautifulSoup
+from validators import ValidationFailure
 
 # from gensim.parsing.preprocessing import remove_stopwords
 
 
 URL = "https://www.zu.ac.ae/main/en/index.aspx"
 all_links = []
+with open('.\\data\\urls_partial.txt', 'w', encoding='UTF8', newline='') as f:
+    f.write('List of Links\n')
+
+with open('.\\data\\urls.txt', 'w', encoding='UTF8', newline='') as f:
+    f.write('List of Links\n')
 
 
 def uri_validator(x):
@@ -16,6 +23,15 @@ def uri_validator(x):
         return all([result.scheme, result.netloc])
     except:
         return False
+
+
+def is_string_an_url(url_string: str) -> bool:
+    result = validators.url(url_string)
+
+    if isinstance(result, ValidationFailure):
+        return False
+
+    return result
 
 
 def link_constructor(partial_link):
@@ -38,35 +54,38 @@ def link_constructor(partial_link):
 
 def find_inner_links(part_data_, all_links_, partial_link):
     try:
-        if uri_validator(partial_link):
+        if uri_validator(partial_link) and is_string_an_url(partial_link):
             page = requests.get(partial_link)
             soup = BeautifulSoup(page.content, "html.parser")
             links = soup.find_all('a', href=True)
-            print(f'all link length before set function: {len(all_links)}')
-
-            if len(part_data_) % 10 == 0 and len(part_data_) > 0:
-                with open('.\\data\\urls_partial.txt', 'a', encoding='UTF8', newline='') as f:
-                    f.writelines(part_data_)
-
-                part_data_ = []
-
             for link in links:
                 discovered_link = link_constructor(link['href'])
                 if len(discovered_link) > 0 \
-                        and discovered_link not in all_links_ \
-                        and 'zu.ac.ae' in discovered_link:
-                    all_links_.append(discovered_link+'\n')
-                    part_data_.append(discovered_link+'\n')
+                        and not discovered_link + '\n' in all_links_ \
+                        and 'zu.ac.ae' in discovered_link \
+                        and uri_validator(discovered_link) and is_string_an_url(discovered_link):
+                    print(f'all link length before set function: {len(all_links)}')
+                    all_links_.append(discovered_link + '\n')
+                    part_data_.append(discovered_link + '\n')
                     print(discovered_link)
+                    if len(part_data_) % 2 == 0 and len(part_data_) > 0:
+                        with open('.\\data\\urls_partial.txt', 'a', encoding='UTF8', newline='') as ft:
+                            ft.writelines(part_data_)
+                            part_data_ = []
                     find_inner_links(part_data_, all_links_, discovered_link)
+
     except TimeoutError:
         print(f'Link Removed, Check for Error : {partial_link}')
         all_links_.remove(all_links_.index(partial_link))
 
 
-    except:
-        print(f' This link gave error : {partial_link}')
-
+    except Exception as e:
+        print(f' This link caused the following error : {e.__cause__}, error class: {e.__class__}, '
+              f'error message {e.__context__}')
+    finally:
+        with open('.\\data\\urls_partial.txt', 'a', encoding='UTF8', newline='') as ft:
+            ft.writelines(part_data_)
+            part_data_ = []
 
 part_data = []
 find_inner_links(part_data, all_links, URL)
